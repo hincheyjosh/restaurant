c2513e0b7baa0b16e3c4cf215a295f9bb9f09b78
Joshua Hinchey <hincheyjosh@gmail.com>
2022-01-05 11:50:18 -0600

    finished menu page

--- src/menuPage.js
+++ src/menuPage.js
@@ -2,6 +2,29 @@ import Menu from "./menu.json"
 
 
 export default function createMenuContent() {
+
+    // Clear out content container
     const content = document.querySelector(".content")
     content.innerHTML = ""
+
+    const menuContainer = document.createElement("div")
+    menuContainer.classList = "menuContainer"
+
+    Menu.forEach(item => {
+        const menuItem = document.createElement("div")
+        menuItem.classList = "menuItem"
+
+        const name = document.createElement("h6")
+        name.classList = "menuName"
+        name.textContent = item.name
+        menuItem.appendChild(name)
+
+        const description = document.createElement("p")
+        description.classList = "menuDescription"
+        description.textContent = item.description
+        menuItem.appendChild(description)
+
+        menuContainer.appendChild(menuItem)
+    })
+    content.appendChild(menuContainer)
 }
